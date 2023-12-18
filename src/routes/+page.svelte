aa985af3f03fcddb21a356495815b808dd5099b4
pashute <pashute@gmail.com>
2023-12-18 17:15:45 +0200

    first looks working continuous

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let paragraph = "lets see";
+    function handleSend(){
+        let resultArea = document.getElementById("resultArea");
+        let resultText = "not yet implemented";
+        if (resultArea) resultArea.value   
+        += "You:" + paragraph + "\n" 
+        + "Overstand: " + resultText + "\n"
+        + "---------" + "\n";
+        let resultsCanvas = document.getElementById("resultsCanvas");
+        if (resultsCanvas) resultsCanvas.getContext("2d").fillText(paragraph, 10, 10);
+        paragraph = "";
+    }
+
+	function handleClear() {
+		let resultArea = document.getElementById("resultArea");
+        if (resultArea) resultArea.value = "";
+        paragraph = "";
+	}
+
+    function handleKeydown(event) {
+        if (event.key === 'Enter') {
+            handleSend();
+        }
+    }
+</script>
+
+<h1>Welcome to SvelteKit Overstand</h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+Please enter your text
+
+<input type="text" id="textInput" bind:value={paragraph} on:keydown={handleKeydown} />
+<br />
+
+<button id="sendBtn" on:click={handleSend}>Send</button> &nbsp;&nbsp; 
+<button id="clearBtn" on:click={handleClear}>Clear</button>
+<hr />
+<textarea name="resultArea" id="resultArea" cols="30" rows="10"></textarea>
+<hr />
+<canvas id="resultsCanvas"></canvas>
